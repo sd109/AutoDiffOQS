267ec01d4190956e87499cc00f3ec1b26fd8be2c
Scott <ilikeducks546@gmail.com>
2022-02-04 16:21:59 +0000

    Use adjoint op instead of dagger

--- src/AutoDiffOQS.jl
+++ src/AutoDiffOQS.jl
@@ -4,8 +4,8 @@ module AutoDiffOQS
 
 using LinearAlgebra, SparseArrays, Zygote, DifferentialEquations, DiffEqSensitivity
 
-export dagger, sigma_x, sigma_y, sigma_z, projection
-export liouvillian, expLt, steady_state, bloch_redfield_steady_state, gibbs_state, gibbs_dist, ode_dynamics
+export sigma_x, sigma_y, sigma_z, projection
+export liouvillian, expLt, steady_state, bloch_redfield_steady_state, pauli_steady_state, gibbs_state, gibbs_dist, ode_dynamics
 
 include("utilities.jl")
 include("time-evolution.jl")
